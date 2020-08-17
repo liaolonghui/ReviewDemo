a0ff5b55819af4166897cb8c67d9d1071b689f9b
liaolonghui <1340881930@qq.com>
2020-08-17 14:12:01 +0800

    小修改

--- 前端复习/other/11.koa的服务端渲染/testCrypto.js
+++ 前端复习/other/11.koa的服务端渲染/testCrypto.js
@@ -1,5 +1,5 @@
 const crypto = require("crypto");
 
 let obj = crypto.createHash('md5');
-obj.upload('123456');  //upload()可以多次调用
-console.log(obj.digest('hex')); 
+obj.update('123456');  //update()可以多次调用
+console.log(obj.digest('hex'));
